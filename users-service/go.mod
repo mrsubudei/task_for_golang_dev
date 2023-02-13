db91d0dab6ec761e61c8712099e0003d27820344
mrsubudei <subudei@gmail.com>
2023-02-13 19:50:48 +0600

    add entity to users-service

---
+++ users-service/go.mod
@@ -0,0 +1,5 @@
+module github.com/mrsubudei/task_for_golang_dev/users-service
+
+go 1.19
+
+require go.mongodb.org/mongo-driver v1.11.1 // indirect
---
+++ users-service/internal/entity/user.go
@@ -0,0 +1,10 @@
+package user
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	Id       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Email    string             `json:"email" bson:"email"`
+	Salt     string             `json:"salt" bson:"salt"`
+	Password string             `json:"password" bson:"password"`
+}
